d682e18ca05d02aa8da54c22f4f6ed98478c1508
Johannes Frank <jmfrank63@gmail.com>
2024-02-05 01:32:58 +0700

    Add files

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import platform
 from pathlib import Path
+import shutil
+import sys
 
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
+from setuptools.command.install import install
 
 _DIR = Path(__file__).parent
 _ESPEAK_DIR = _DIR / "install"
@@ -11,6 +14,47 @@ _LIB_DIR = _DIR / "lib" / f"Linux-{platform.machine()}"
 _ONNXRUNTIME_DIR = _LIB_DIR / "onnxruntime"
 
 __version__ = "1.2.0"
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command to copy espeak-ng-data and specific DLLs."""
+
+    def run(self):
+        super().run()
+
+        # This custom installation step is only necessary for Windows
+        if platform.system() == "Windows":
+            self.copy_espeak_ng_data()
+            self.copy_specific_dlls()
+
+    def copy_espeak_ng_data(self):
+        source_data_dir = _ESPEAK_DIR / "share" / "espeak-ng-data"
+        target_data_dir = Path(sys.prefix) / "Lib" / "site-packages" / "piper_phonemize" / "espeak-ng-data"
+
+        if source_data_dir.exists():
+            if target_data_dir.exists():
+                shutil.rmtree(target_data_dir)
+            shutil.copytree(source_data_dir, target_data_dir)
+            print(f"Copied espeak-ng-data to: {target_data_dir}")
+
+    def copy_specific_dlls(self):
+        dll_files = [
+            _ESPEAK_DIR / "bin" / "espeak-ng.dll",
+            _ESPEAK_DIR / "lib" / "onnxruntime.dll"
+        ]
+
+        target_lib_dir = Path(sys.prefix) / "Library" / "bin" # Windows specific
+        if not target_lib_dir.exists():
+            print(f"Error: {target_lib_dir} does not exist and DLLs were not copied.")
+            exit(1)
+
+        for dll_path in dll_files:
+            if dll_path.exists():
+                shutil.copy(dll_path, target_lib_dir)
+                print(f"Copied {dll_path} to: {target_lib_dir}")
+            else:
+                print(f"Error: {dll_path} does not exist and was not copied.")
+                exit(1)
+
 
 ext_modules = [
     Pybind11Extension(
